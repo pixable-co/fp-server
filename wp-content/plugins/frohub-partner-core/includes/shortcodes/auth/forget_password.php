c7af4edfbc3e8e1e7a1cfe9630de4befba5b7b5e
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-05-21 20:01:57 +0600

    Updated

--- wp-content/plugins/frohub-partner-core/includes/shortcodes/auth/forget_password.php
+++ wp-content/plugins/frohub-partner-core/includes/shortcodes/auth/forget_password.php
@@ -9,59 +9,60 @@ class ForgetPassword {
 
     public static function init() {
         $self = new self();
+
+        // Register shortcode
         add_shortcode( 'forget_password', array( $self, 'render_forgot_password_form' ) );
     }
 
     public function render_forgot_password_form() {
         ob_start();
 
-        if ( isset($_POST['fp_forgot_password_nonce']) && wp_verify_nonce($_POST['fp_forgot_password_nonce'], 'fp_forgot_password_action') ) {
-            $user_login = sanitize_text_field( $_POST['user_login'] );
-
-            if ( empty($user_login) ) {
-                $error = 'Please enter your username or email.';
-            } else {
-                $user = get_user_by( 'login', $user_login );
-                if ( ! $user ) {
-                    $user = get_user_by( 'email', $user_login );
-                }
-
-                if ( $user ) {
-                    $reset_key = get_password_reset_key( $user );
-                    if ( is_wp_error($reset_key) ) {
-                        $error = 'Could not generate reset link. Please try again.';
-                    } else {
-                        $reset_url = network_site_url( "wp-login.php?action=rp&key=$reset_key&login=" . rawurlencode( $user->user_login ), 'login' );
-                        wp_mail(
-                            $user->user_email,
-                            'Password Reset Request',
-                            "Click the link below to reset your password:\n\n" . $reset_url
-                        );
-                        $success = 'A password reset link has been sent to your email.';
-                    }
-                } else {
-                    $error = 'No user found with that username or email.';
-                }
-            }
-        }
+        $nonce = wp_create_nonce('fpserver_nonce');
+        $ajax_url = admin_url('admin-ajax.php');
 
         ?>
-        <form method="post" class="fp-forgot-password-form">
-            <?php if ( isset($error) ) : ?>
-                <p style="color: red;"><?php echo esc_html($error); ?></p>
-            <?php elseif ( isset($success) ) : ?>
-                <p style="color: green;"><?php echo esc_html($success); ?></p>
-            <?php endif; ?>
-
+        <form class="fp-forgot-password-form" method="post" onsubmit="return false;">
+            <div class="fp-message" style="margin-bottom: 1em;"></div>
             <p>
                 <label for="user_login">Username or Email</label><br>
                 <input type="text" name="user_login" id="user_login" required>
             </p>
             <p>
-                <input type="hidden" name="fp_forgot_password_nonce" value="<?php echo wp_create_nonce('fp_forgot_password_action'); ?>">
+                <input type="hidden" name="fp_nonce" value="<?php echo esc_attr($nonce); ?>">
                 <button type="submit">Send Reset Link</button>
             </p>
         </form>
+
+        <script>
+        jQuery(document).ready(function ($) {
+            const form = $('.fp-forgot-password-form');
+            const messageBox = form.find('.fp-message');
+
+            form.on('submit', function (e) {
+                e.preventDefault();
+
+                messageBox.text('').css('color', '');
+
+                const user_login = form.find('[name="user_login"]').val();
+                const nonce = form.find('[name="fp_nonce"]').val();
+
+                jQuery.post('<?php echo esc_url($ajax_url); ?>', {
+                    action: 'fpserver/forgot_password',
+                    _ajax_nonce: nonce,
+                    user_login: user_login
+                }, function (response) {
+                    if (response.success) {
+                        messageBox.css('color', 'green').text(response.data.message);
+                        form[0].reset();
+                    } else {
+                        messageBox.css('color', 'red').text(response.data.message);
+                    }
+                }).fail(function () {
+                    messageBox.css('color', 'red').text('Something went wrong. Please try again.');
+                });
+            });
+        });
+        </script>
         <?php
 
         return ob_get_clean();
